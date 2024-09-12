d9ecf93878bd9d9f672f88351cbf987494d1205f
pavan kumar <80616171+pavankumar-ui@users.noreply.github.com>
2024-09-12 16:13:04 +0530

    Updated NavContainer.jsx

--- src/Views/NavContainer.jsx
+++ src/Views/NavContainer.jsx
@@ -1,29 +1,29 @@
-import { useState } from "react";
+
+import { useEffect } from "react";
 import isUserAuth from "../Utils/helpers";
+import PropTypes from 'prop-types';
+import Cart from "./Cart";
 
+function NavContainer({ cartItem, setCartItem }) {
 
-function NavContainer() {
+    const handleCartItem = () => {
+        setCartItem(prevCartItem => prevCartItem + 1);
+        console.log(`Cart Items are:${cartItem}`);
 
-
-
-    const [cartItem, setCartItem] = useState(0);
-
-
-    const handleCartItem = (e) => {
-        e.preventDefault();
-
-        if (isUserAuth()) {
-            setCartItem(cartItem + 1);
-        }
     }
 
+
+
+    /*useEffect(() => {
+        if (isUserAuth() && Cart) {
+            handleCartItem();
+        }
+    });*/
+
     return (
-
-
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
             <div className="container-fluid">
                 <a className="navbar-brand" href="/products">
-
                     <img src="src/assets/Images/Company_logo.png"
                         alt="logo"
                         className="img-rounded-circle"
@@ -33,7 +33,6 @@ function NavContainer() {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
-
 
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
@@ -45,11 +44,9 @@ function NavContainer() {
                             <a className="nav-link active" aria-current="page" href="/products/about">About</a>
                         </li>
 
-
-                        {isUserAuth() ? (<li className="nav-item">
-                            <a className="nav-link" href="/Cart">Cart
+                        {isUserAuth() && Cart ? (<li className="nav-item">
+                            <a className="nav-link" href="/products/cart">Cart
                                 <i className="fa fa-shopping-cart">
-
                                     <span style={{ padding: "4px", margin: "1.5px" }}
                                         className="badge rounded-pill text-bg-danger"
                                         onChange={(e) => { handleCartItem(e) }}
@@ -65,7 +62,7 @@ function NavContainer() {
                                 Wishlist</a>
                         </li>) : ''}
 
-                       {isUserAuth() ? (
+                        {isUserAuth() ? (
                             <li className="nav-item dropdown">
                                 <a className="nav-link dropdown-toggle text-primary" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                     <i className="fa fa-user"></i>&nbsp;Profile
@@ -83,21 +80,25 @@ function NavContainer() {
                                         Logout</a></li>
                                 </ul>
                             </li>
-                        ) : (<li className="nav-item">
+                        ) : (
+                            <li className="nav-item">
                                 <button className="btn btn-info" onClick={() => {
                                     window.location.href = '/Login';
                                 }}>
                                     <i className="fa fa-lock"></i> Login
                                 </button>
-                            </li>)}
-
-
+                            </li>
+                        )}
                     </ul>
                 </div>
             </div>
         </nav>
-
-
     );
 }
+
+NavContainer.propTypes = {
+    cartItem: PropTypes.number.isRequired,
+    setCartItem: PropTypes.func.isRequired,
+};
+
 export default NavContainer;
